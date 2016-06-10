802572aea756ed019de61d27e3d164d434a61ead
Kevin Bui <kvb2univpitt@gmail.com>
2016-06-10 17:08:56 -0400

    Create Shiro password object as a bean.

---
+++ src/main/java/edu/pitt/dbmi/ccd/causal/rest/api/conf/ShiroConfigurer.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2016 University of Pittsburgh.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package edu.pitt.dbmi.ccd.causal.rest.api.conf;
+
+import org.apache.shiro.authc.credential.DefaultPasswordService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * Jun 10, 2016 3:52:25 PM
+ *
+ * @author Kevin V. Bui (kvb2@pitt.edu)
+ */
+@Configuration
+public class ShiroConfigurer {
+
+    @Bean
+    public DefaultPasswordService defaultPasswordService() {
+        return new DefaultPasswordService();
+    }
+
+}
