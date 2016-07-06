df72d22d37781220c7ec8ec4600bc05f6a60d9d6
Zhou Yuan <yuanzhou19@gmail.com>
2016-07-06 12:12:26 -0400

    Added list all available algorithms

---
+++ src/main/java/edu/pitt/dbmi/ccd/causal/rest/api/dto/AlgorithmDTO.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2016 University of Pittsburgh.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package edu.pitt.dbmi.ccd.causal.rest.api.dto;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Zhou Yuan (zhy19@pitt.edu)
+ */
+@XmlRootElement(name = "Algorithm")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class AlgorithmDTO {
+
+    @XmlElement
+    private int id;
+
+    @XmlElement
+    private String name;
+
+    @XmlElement
+    private String description;
+
+    public AlgorithmDTO() {
+    }
+
+    public AlgorithmDTO(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
---
+++ src/main/java/edu/pitt/dbmi/ccd/causal/rest/api/service/AlgorithmEndpointService.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2016 University of Pittsburgh.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package edu.pitt.dbmi.ccd.causal.rest.api.service;
+
+import edu.pitt.dbmi.ccd.causal.rest.api.dto.AlgorithmDTO;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Zhou Yuan (zhy19@pitt.edu)
+ */
+@Service
+public class AlgorithmEndpointService {
+
+    private static final List<AlgorithmDTO> ALGORITHMS = new LinkedList<>();
+
+    static {
+        ALGORITHMS.add(new AlgorithmDTO(1, "fgs", "FGS continuous"));
+        ALGORITHMS.add(new AlgorithmDTO(2, "fgs-discrete", "FGS discrete"));
+    }
+
+    /**
+     * List all the available algorithms
+     *
+     * @return A list of available algorithms
+     * @throws IOException
+     */
+    public List<AlgorithmDTO> listAlgorithms() throws IOException {
+        return ALGORITHMS;
+    }
+}
